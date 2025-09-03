a7da04dcf6300cc24088cf3dafef2f439c6388bc
Thiago Scheffer Fialho <86134238+ThiagoSchFialho@users.noreply.github.com>
2025-09-03 05:43:11 -0300

    initial setup

---
+++ src/routes/index.ts
@@ -0,0 +1,10 @@
+import { Request, Response } from 'express';
+var express = require('express');
+var router = express.Router();
+
+/* GET home page. */
+router.get('/', function(req: Request, res: Response, next: any) {
+  res.render('index', { title: 'Express' });
+});
+
+module.exports = router;
